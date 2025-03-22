d474f0adc1ecefe08132200fcf67aaaf80b5c5c0
kempensp <kempensp@milwaukee.k12.wi.us>
2025-03-22 14:40:11 +0000

    Working on password encryption

--- hsccinfo/app.js
+++ hsccinfo/app.js
@@ -13,7 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bdpahistoryRouter = require('./routes/bdpahistory');
 var formentryRouter = require('./routes/formentry');
-
+var registerTestRouter = require('./routes/registertest');
 // Setting up Express app
 var app = express();
 
@@ -35,6 +35,7 @@ app.use('/', indexRouter);  //Note that index router connects with /.
 app.use('/users', usersRouter);
 app.use('/bdpahistory', bdpahistoryRouter);
 app.use('/formentry', formentryRouter);
+app.use('/registertest', registerTestRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
