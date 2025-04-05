3823af1fe21f56e62786e3e54708db7ae278ed32
kempensp <kempensp@milwaukee.k12.wi.us>
2025-04-05 16:14:15 +0000

    First April 5 updated

--- hsccinfo/routes/createelection.js
+++ hsccinfo/routes/createelection.js
@@ -12,6 +12,19 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
     if (process.env.CONSOLE_DEBUG=="true"){
       console.log(req.body);
+      console.log(req.body.openingtime);
+    }
+
+    //Create Date objects for opening and closing time of elections
+    //and convert to unix epoch milliseconds to post to API
+    openingdate=new Date(req.body.openingtime);
+    openingmilliseconds=openingdate.getTime();
+    closingdate=new Date(req.body.closingtime);
+    closingmilliseconds=closingdate.getTime();
+
+    if (process.env.CONSOLE_DEBUG=="true"){
+      console.log(openingmilliseconds);
+      console.log(closingmilliseconds);
     }
     res.render('createelection', { title: 'Create an Election' });
   });
