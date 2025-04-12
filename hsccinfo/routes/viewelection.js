27c06695a6a73257f938200290c12ccb632331d7
kempensp <kempensp@milwaukee.k12.wi.us>
2025-04-12 16:06:38 +0000

    View individual election

--- hsccinfo/routes/viewelection.js
+++ hsccinfo/routes/viewelection.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const APIRequests=require("../middleware/APIRequests");
 
-// GET Electionsmeta page.
+// GET viewelection general page.
 router.get('/', function(req, res, next) {
 
     const url = 'https://elections-cpl.api.hscc.bdpa.org/v1/elections'
@@ -32,5 +32,37 @@ router.get('/', function(req, res, next) {
     }) // data then component
     .catch(error => console.error(error));
 });
+
+// GET viewelection route for a specific election given its id
+router.get('/:election_id', function(req, res, next) {
+
+    const url = 'https://elections-cpl.api.hscc.bdpa.org/v1/elections/' + req.params.election_id
+    const token = process.env.BEARER_TOKEN;
+   
+    // Pass url and token into RestAPIGet and pull information from response
+    APIRequests.getWithBearerToken(url, token)
+    .then(data => {
+        if (process.env.CONSOLE_DEBUG=="true"){
+            console.log("REST CALL ", data);
+        }
+        
+        if (data.success){
+            // SUBJECT TO CHANGE
+            var Election=data.election;
+            res.render('viewelectionsingle', {
+                title: 'Election information',
+                Electiondata: Election,
+            })
+        } // closes if statement
+
+        else{
+            res.render('error', {title: 'Elections call failed',
+            message: data.error,
+            });
+        }
+    }) // data then component
+    .catch(error => console.error(error));
+});
+
 
 module.exports = router;
