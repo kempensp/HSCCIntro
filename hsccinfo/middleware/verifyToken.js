3d90cfee19ca61a1aa638ea2239b56115ba6163f
kempensp <kempensp@milwaukee.k12.wi.us>
2025-05-10 17:21:13 +0000

    ,menu

--- hsccinfo/middleware/verifyToken.js
+++ hsccinfo/middleware/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const token =
     req.body.token || req.query.token || req.headers["x-access-token"] || global.userToken;
-  console.log(token)
+  
   if (!token) {
     res.locals.role="guest";
     //return res.status(403).send("A token is required for authentication");
